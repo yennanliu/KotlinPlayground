795d07aeb06df7bdc6b6b05d5789b98d8892a0c5
yennanliu <f339339@gmail.com>
2025-03-12 20:37:31 +0800

    add

---
+++ KotlinSpringBoot1/src/main/kotlin/com/yen/KotlinSpringBoot1/KotlinSpringBoot1Application.kt
@@ -0,0 +1,11 @@
+package com.yen.KotlinSpringBoot1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringBoot1Application
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringBoot1Application>(*args)
+}
---
+++ KotlinSpringBoot1/src/test/kotlin/com/yen/KotlinSpringBoot1/KotlinSpringBoot1ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yen.KotlinSpringBoot1
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringBoot1ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
